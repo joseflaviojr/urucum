4ee172f01fa123b3f3a3c8e9b338cfe2fbcbac3f
Jose Flavio de Souza Dias Junior <contato@joseflavio.com>
2018-02-28 21:59:28 -0300

    Atualização da URL http://joseflavio.com/urucum/

--- fonte/com/joseflavio/urucum/tucurui/TucuruiImpressora.java
+++ fonte/com/joseflavio/urucum/tucurui/TucuruiImpressora.java
@@ -2,7 +2,7 @@
 /*
  *  Copyright (C) 2016-2018 José Flávio de Souza Dias Júnior
  *  
- *  This file is part of Urucum - <http://www.joseflavio.com/urucum/>.
+ *  This file is part of Urucum - <http://joseflavio.com/urucum/>.
  *  
  *  Urucum is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -21,7 +21,7 @@
 /*
  *  Direitos Autorais Reservados (C) 2016-2018 José Flávio de Souza Dias Júnior
  * 
- *  Este arquivo é parte de Urucum - <http://www.joseflavio.com/urucum/>.
+ *  Este arquivo é parte de Urucum - <http://joseflavio.com/urucum/>.
  * 
  *  Urucum é software livre: você pode redistribuí-lo e/ou modificá-lo
  *  sob os termos da Licença Pública Menos Geral GNU conforme publicada pela
